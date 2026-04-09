f85d34975cb06011bfada34036be424d02ade435
Jason Billingham <jason@billingham.ca>
2026-04-09 09:07:00 -0400

    Update dotfiles: 2026-04-09 09:07:00

--- README.md
+++ README.md
@@ -1,10 +1,38 @@
 # dotfiles
-These are my dotfiles that I've linked using stow, and uploaded to GitHub.
 
-I've added these here mostly for my own ease of use, but feel free to copy and use them if they are useful to you.
+## Disclaimer
+----------
+
+These are my dotfiles that I've customized for my own use, and uploaded to GitHub to make my life easier when re-installing my system.
+
+I've added these here mostly for my own ease of use, but wanted to allow others to use them as an example configuration, or starting configuration.
 
 I do not guarantee in any way that these files are free from error, or that they won't cause you issues on your own system.
 
 Use these files at your own risk.
 
-![Screenshot](/screenshots/2025-07-19-121756_hyprshot.png)
+## Prerequisites:
+
+These files were created on an Arch Linux system running Hyprland. In order to ensure that everything works, I recommend the same configuration.
+
+## Installation
+
+If you want to install these on an Arch/Hyprland system, you can must first use the **installSoftware.sh** script.
+
+Once the software has successfully installed, run the **restoreDotfiles.sh** script.
+
+### This will perform the following actions:
+
+1. Update your system using **pacman -Syu**
+2. Install required development packages: **git and base-devel**
+3. Install YAY from AUR if not already installed.
+4. Install required software.
+5. Ask you about optional software, and install.
+
+### Other Distributions
+
+If you are trying to use these on a different distribution of Linux, you'll instead want to manually copy the configuration from each folder, into the appropriate location for your distro.
+
+## Backup
+
+The **backupDotfiles.sh** script will copy the configuration from each of the folders listed into a folder called ~/dotfiles.  Unless you create your own github account/repository and add it to that location, the git commands will fail. Setting up a git repo is out of scope for this document.
--- installSoftware.sh
+++ installSoftware.sh
@@ -20,10 +20,10 @@ fi
 echo "--- Installing Core Software Suite ---"
 yay -S --needed hypridle hyprlock hyprpaper waybar swaync rofi hyprshot wttrbar \
 nerd-fonts-noto-sans-mono ttf-font-awesome ttf-cascadia-code-nerd \
-noto-fonts-emoji ttf-jetbrains-mono-nerd pavucontrol onedrive-abraunegg \
-onedrivegui discord libreoffice-fresh google-chrome gimp vlc fastfetch \
-keepassxc thunderbird spotify gwenview bluez bluetooth-manager \
-webcamize power-profiles-daemon rsync
+noto-fonts-emoji ttf-jetbrains-mono-nerd pavucontrol \
+libreoffice-fresh gimp vlc fastfetch \
+gwenview bluez bluetooth-manager \
+power-profiles-daemon rsync
 
 # Function to handle conditional installs
 ask_install() {
@@ -37,6 +37,12 @@ ask_install() {
 
 echo "--- Optional Components ---"
 
+# Do you want to use OneDrive?
+ask_install "Do you want to use OneDrive?" "onedrive-abraunegg onedrivegui"
+
+# Do you want to use Discord?
+ask_install "Do you want to use Discord?" "discord"
+
 # Will you be using a streamdeck on this pc?
 ask_install "Will you be using a Stream Deck on this PC?" "opendeck"
 
@@ -46,7 +52,22 @@ ask_install "Does this PC have RGB lighting?" "openrgb"
 # Will you be gaming on this PC?
 ask_install "Will you be gaming on this PC?" "steam lutris"
 
-# Will you be using this PC for work?
-ask_install "Will you be using this PC for work?" "microsoft-edge-stable-bin"
+# Do you want to install Thunderbird for email?
+ask_install "Do you want to install Thunderbird for email?" "thunderbird"
+
+# Do you want to install keepassxc?
+ask_install "Do you want to install keepassxc?" "keepassxc"
+
+# Do you want to install Spotify?
+ask_install "Do you want to install Spotify?" "spotify"
+
+# Do you want to install Google Chrome?
+ask_install "Do you want to install Google Chrome?" "google-chrome"
+
+# Do you want to install Microsoft Edge?
+ask_install "Do you want to install Microsoft Edge?" "microsoft-edge-stable-bin"
+
+# Will you be using a webcam?
+ask_install "Will you be using a webcam?" "webcamize"
 
 echo "--- Installation Complete! ---"
